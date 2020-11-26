ef0bb9b88f5e9ade1663020202206463facde939
Anysh <anysh@Odyssey.local>
2020-11-26 21:15:03 +0530

    commit on 'master'

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+        <link rel="stylesheet" href="css/styles.css">
+        <script src="js/jquery-3.5.1.js"></script>
+        <script src="js/header.js"></script>
+    </head>
+    <body>
+        <div class="header">
+            <div class="align-header-text">
+                <table>
+                    <tr>
+                        <th><button class = "header-account logo"><strong>baked.</strong></button> </th>
+                        <th id="headerButton1"> Home</th>
+                        <th id="headerButton2">Products</th>
+                        <th id="headerButton3">Pricing</th>
+                        <th id="headerButton4">Blogs</th>
+                        <th id="headerButton5">About</th>
+                        <th><button id="headerButton6" class = "header-account accountButton account-spacing">Login</button></th>
+                        <th><button id="headerButton7" class = "header-account accountButton">Register</button></th>
+                    </tr>
+                </table>
+                <hr>
+            </div>
+        </div>
+        <div class="body-text-compress">
+            <strong>Compress video like a </strong> <strong class = "color-tag">Pro</strong>
+            <div> <strong>with </strong><strong class = "color-tag">baked.</strong> </div>
+        </div>
+    </body>
+</html>
---
+++ js/header.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    $("#headerButton1").click(function()
+    {
+        alert("You're in a Home Page!");
+    });
+
+    $("#headerButton1").mouseover(function(){
+        $("#headerButton1").css("color", "#449aa0");
+    });
+    $("#headerButton1").mouseout(function(){
+        $("#headerButton1").css("color", "grey");
+    });
+
+    $("#headerButton2").mouseover(function(){
+        $("#headerButton2").css("color", "#449aa0");
+    });
+    $("#headerButton2").mouseout(function(){
+        $("#headerButton2").css("color", "grey");
+    });
+
+    $("#headerButton3").mouseover(function(){
+        $("#headerButton3").css("color", "#449aa0");
+    });
+    $("#headerButton3").mouseout(function(){
+        $("#headerButton3").css("color", "grey");
+    });
+
+    $("#headerButton4").mouseover(function(){
+        $("#headerButton4").css("color", "#449aa0");
+    });
+    $("#headerButton4").mouseout(function(){
+        $("#headerButton4").css("color", "grey");
+    });
+
+    $("#headerButton5").mouseover(function(){
+        $("#headerButton5").css("color", "#449aa0");
+    });
+    $("#headerButton5").mouseout(function(){
+        $("#headerButton5").css("color", "grey");
+    });
+
+    $("#headerButton6").mouseover(function(){
+        $("#headerButton6").css("background-color", "#264547");
+        $("#headerButton6").css("color", "#449aa0");
+    });
+    $("#headerButton6").mouseout(function(){
+        $("#headerButton6").css("background-color", "#0d0d0d");
+        $("#headerButton6").css("color", "#449aa0");
+    });
+
+    $("#headerButton7").mouseover(function(){
+        $("#headerButton7").css("background-color", "#264547");
+        $("#headerButton7").css("color", "#449aa0");
+    });
+    $("#headerButton7").mouseout(function(){
+        $("#headerButton7").css("background-color", "#0d0d0d");
+        $("#headerButton7").css("color", "#449aa0");
+    });
+});
+
